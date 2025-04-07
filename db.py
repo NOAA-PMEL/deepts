2b5f0aa74f2f18f9071fbaf7ddad44ee5fe593ef
workspaceuser <workspaceuser@plot.ly>
2025-04-07 22:10:37 +0000

    update to pandas 2.2 connection method

--- db.py
+++ db.py
@@ -8,10 +8,11 @@ def get_between(site_code, since1, since2):
         return
     selection = 'SELECT * from {} WHERE SITE_CODE = \'' + site_code + '\' AND TIME BETWEEN \'' + since1 + '\' AND \'' + since2 + '\' ORDER BY TIME';
     print(selection.format(constants.data_table))
-    stored_df = pd.read_sql(
-        selection.format(constants.data_table), constants.postgres_engine
-    )
-    return stored_df
+    with constants.postgres_engine.connect() as conn:
+        stored_df = pd.read_sql(
+            selection.format(constants.data_table), con=conn.connection
+        )
+        return stored_df
 
 def get_by_stride(site_code, total_est, start_date, end_date):
     millis1 = start_date*1000000000
@@ -27,26 +28,30 @@ def get_by_stride(site_code, total_est, start_date, end_date):
 def get_some(fraction):
     selection = 'SELECT * FROM {} TABLESAMPLE BERNOULLI(' + str(fraction) + ');'
     print(selection.format(constants.data_table))
-    stored_df = pd.read_sql(
-        selection.format(constants.data_table), constants.postgres_engine
-    )
-    return stored_df
+    with constants.postgres_engine.connect() as conn:
+        stored_df = pd.read_sql(
+            selection.format(constants.data_table), con=conn.connection
+        )
+        return stored_df
 
 def count():
     selection = 'SELECT COUNT(*) FROM {};'
     print(selection.format(constants.data_table))
-    stored_df = pd.read_sql(
-        selection.format(constants.data_table), constants.postgres_engine
-    )
-    return stored_df
+    with constants.postgres_engine.connect() as conn:
+        stored_df = pd.read_sql(
+            selection.format(constants.data_table), con=conn.connection
+        )
+        return stored_df
 
 def version():
-    v = pd.read_sql("SELECT VERSION()", constants.postgres_engine)
-    print(v)
+    with constants.postgres_engine.connect() as conn:
+        v = pd.read_sql("SELECT VERSION()", con=conn.connection)
+        print(v)
 
 
 def get_locations():
     selection = "SELECT * FROM {}"
     selection = selection.format(constants.location_table)
-    locations_df = pd.read_sql(selection, constants.postgres_engine)
-    return locations_df
+    with constants.postgres_engine.connect() as conn:
+        locations_df = pd.read_sql(selection, con=conn.connection)
+        return locations_df
